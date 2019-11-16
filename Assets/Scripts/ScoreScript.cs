f27860c6ae764000ae74d9a9d9fa5b39388713fc
jpb1394 <jpb1394@rit.edu>
2019-11-16 14:26:59 -0500

    Score Script
    Pushing before I really start to break things--
    A couple things of note here: A new script attached to the top level canvas called ScoreScript has been made-- this is for score calculations, and for holding all the player's progress through a round.
    Also, right now if you click too quickly through the fledgling screens the game will crash. This is problematic.
    Also, I renamed fledgling and mister to "apprentice" and "chef" respectively, because it was way easier to make work in code.

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+    public int totalScore;
+
+    private int pointsCorrectIngredient;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalScore = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Calculates the score to add to the player's final score.
+    /// TODO change how this is calculated. Currently this just adds 20 points every correct ingredient chosen
+    /// </summary>
+    /// <param name="recipe"></param>
+    /// <param name="chosen"></param>
+    public void AddScore(List<string> recipe, List<string> chosen)
+    {
+        for (int i = 0; i < recipe.Count; i++)
+        {
+            if (chosen.Contains(recipe[i]))
+                totalScore += pointsCorrectIngredient;
+        }
+    }
+
+
+    /// <summary>
+    /// Returns the player's total score-- mostly called by the UIManager to keep the total score updated
+    /// </summary>
+    /// <returns></returns>
+    public int GetTotalScore()
+    {
+        return totalScore;
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,6 +9,10 @@ public class UIManager : MonoBehaviour
     public Text recipeName; // The title of the chef canvas
     public List<Text> apprenticeIngredientList; // The list of ingredients presented to an apprentice at the start of their turn in a round
 
+    public Text confirmScreenText; // The words that appear on the confirmation screen, telling players to pass the phone to the next player
+    public GameObject confirmScreenToChef;  // The button that thakes the player to the Chef's view. Corresponds to 0
+    public GameObject confirmScreenToApprentice;    // The button that takes the player to the Apprentice's view. Corresponds to 1
+
     private RecipeGenerator RG;
 
     // Start is called before the first frame update
@@ -36,6 +40,10 @@ public class UIManager : MonoBehaviour
         int x = 0;
         foreach (string ingredient in recipe)
         {
+            if (chefsRecipeList[x] == null)
+            {
+                break;
+            }
             chefsRecipeList[x].text = ingredient;
             x++;
         }
@@ -56,6 +64,43 @@ public class UIManager : MonoBehaviour
         {
             apprenticeIngredientList[x].text = ingredient;
             x++;
+        }
+    }
+
+    /// <summary>
+    /// Called when an apprentice selects an ingredient to add to the juice
+    /// </summary>
+    public void IngredientSelected(Text ingredientName)
+    {
+        RG.AddChosenIngredient(ingredientName.text);
+
+        // If we have completed this recipe, we need to set our confirmation screen to take play back to the chef's screen
+        if (RG.CheckRecipeCompletion())
+        {
+            SetConfirmScreenButton(0);
+        }
+    }
+
+    /// <summary>
+    /// Sets which button will be active on the confirm screen-- this will decide whether the chef screen will be shown, or the apprentice screen will be shown.
+    /// Passing in 0 here will take players to the chef screen
+    /// Passing in 1 here will take players to the Apprentice screen
+    /// </summary>
+    /// <param name="button"></param>
+    public void SetConfirmScreenButton(int button)  //THIS ISNT BEING CALLED WHEN THE PLAYER HITS START ON THE CHEF SCREEN?
+    {
+        switch (button)
+        {
+            case 0:
+                confirmScreenToChef.SetActive(true);
+                confirmScreenToApprentice.SetActive(false);
+                confirmScreenText.text = "New Recipe! Pass to Juice Miser";
+                break;
+            case 1:
+                confirmScreenToChef.SetActive(false);
+                confirmScreenToApprentice.SetActive(true);
+                confirmScreenText.text = "Pass to Next Fledgling";
+                break;
         }
     }
 }
